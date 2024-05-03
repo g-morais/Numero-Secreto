412ffe07993c0d08025ee2706e570f995539d4c3
Guilherme Willam Morais <guilhermem750@gmail.com>
2024-05-03 20:23:31 -0300

    exercicio de lógica

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-alert("loucura no caiderao");
-let numeroSecreto = 0;
+alert("Bem vindo ao jogo, tente acertar o numero e ganhe um abraço!");
+var numeroSecreto = 17;
 
-//test commit
+var Chance = prompt('escolha um numero entre 1 e 35');
+
+console.log("o numero secreto é o ", numeroSecreto)
+
+ 
+
+if ( Chance == 17 ){
+
+    alert('boa amigo você conseguiu! o numero secreto ero o ' + numeroSecreto + 'toma o teu abreço virtual <3');
+}
+
+else {
+    if  ( numeroSecreto > Chance) {
+        alert("O numero secreto é maior que o chute")
+}
+    else {
+        alert("o numero secreto é menor");
+    }
+}
