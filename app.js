70b474c7b5accce9e1042bf147c2d6d8d83da3e9
Guilherme Willam Morais <guilhermem750@gmail.com>
2024-05-03 20:44:50 -0300

    looping adicionado

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 alert("Bem vindo ao jogo, tente acertar o numero e ganhe um abraço!");
 var numeroSecreto = 17;
 
-var Chance = prompt('escolha um numero entre 1 e 35');
+var Chance;
 
-console.log("o numero secreto é o ", numeroSecreto)
+console.log("o numero secreto é o ", numeroSecreto);
 
- 
-
-if ( Chance == 17 ){
-
-    alert('boa amigo você conseguiu! o numero secreto ero o ' + numeroSecreto + 'toma o teu abreço virtual <3');
-}
-
-else {
-    if  ( numeroSecreto > Chance) {
-        alert("O numero secreto é maior que o chute")
-}
-    else {
-        alert("o numero secreto é menor");
+//looping
+while (Chance != numeroSecreto) {
+  Chance = prompt("escolha um numero entre 1 e 35");
+  // condicional de numero
+  if (Chance == 17) {
+    alert("boa amigo você conseguiu! o numero secreto ero o " +numeroSecreto + " toma o teu abraço virtual <3" );
+  } else {
+    if (numeroSecreto > Chance) {
+      alert("O numero secreto é maior que o chute:" + Chance);
+    } else {
+      alert("o numero secreto é menor que " + Chance);
     }
+  }
 }
