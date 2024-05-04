43c61ac231ef2289bdbf4d67220e74bc9b26d9e2
Guilherme Willam Morais <guilhermem750@gmail.com>
2024-05-03 21:23:59 -0300

    projeto estudo finalizado

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 alert("Bem vindo ao jogo, tente acertar o numero e ganhe um abraço!");
-var numeroSecreto = 17;
-
+var numeroMaximo = 300;
+var numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 var Chance;
+var tentativas = 1;
 
 console.log("o numero secreto é o ", numeroSecreto);
 
 //looping
 while (Chance != numeroSecreto) {
-  Chance = prompt("escolha um numero entre 1 e 35");
+  Chance = prompt("escolha um numero entre 1 e " + numeroMaximo);
   // condicional de numero
-  if (Chance == 17) {
-    alert("boa amigo você conseguiu! o numero secreto ero o " +numeroSecreto + " toma o teu abraço virtual <3" );
+  if (Chance == numeroSecreto) {
+    break;
   } else {
     if (numeroSecreto > Chance) {
       alert("O numero secreto é maior que o chute:" + Chance);
     } else {
       alert("o numero secreto é menor que " + Chance);
     }
+    tentativas++;
   }
 }
+alert("boa amigo você conseguiu! o numero com " +tentativas+ " tentativas, o numero secreto ero o " +numeroSecreto + " toma o teu abraço virtual <3" );
